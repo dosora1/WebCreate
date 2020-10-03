8ff5875ed8ecc4573c934d7022c3052f1db3d131
dosora1 <61692372+dosora1@users.noreply.github.com>
2020-10-04 03:11:17 +0900

    Update ytb.js

--- ytb.js
+++ ytb.js
@@ -2,6 +2,13 @@ var suffix=1;
 function downloadAll(oFrm){
   var oChk = oFrm.elements["file" + (suffix++)];
   if (oChk){
-    location.href="file:///C:/Users/Admin/Desktop/programming/WebCreate/download.html"
+    if (oChk.checked)
+      location.href="https://dosora1.github.io/WebCreate/download" + oChk.value;
+      setTimeout(function()){
+        downloadAll(oFrm)
+      },1000);
+  } else{
+      downloadAll(oFrm);
+  }
   }
 }
